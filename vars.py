59035ca330e221bdfd00ba8aa3eef07aa484d19a
gauravyt566-wq <gauravyt566@gmail.com>
2025-09-13 08:35:41 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,4 +1,3 @@
-#fill all vars with your value 
-owner = [your_id] #you can add multiple ids using [7704212317, 8334457175]
-#bot token 
-bot_token = "8256772577:AAFKJwkwQcuKruUzx81fPGjGpfx6GZIpFQQ"
+import os
+owner = [int(os.getenv("7704212317"))]
+bot_token = os.getenv("8256772577:AAFKJwkwQcuKruUzx81fPGjGpfx6GZIpFQQ")
