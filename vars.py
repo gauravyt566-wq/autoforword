4b44500ec717441fcf69103afc4a6762d11690fa
gauravyt566-wq <gauravyt566@gmail.com>
2025-09-13 08:19:15 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,4 +1,4 @@
 #fill all vars with your value 
-owner = [your_id] #you can add multiple ids using [123456789, 987654321]
+owner = [your_id] #you can add multiple ids using [7704212317, 8334457175]
 #bot token 
-bot_token = "yourbot token here"
+bot_token = "8256772577:AAFKJwkwQcuKruUzx81fPGjGpfx6GZIpFQQ"
